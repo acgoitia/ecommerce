eb88570154e9fb2483dabab49952cd03fd79f2fa
acgoitia <acgoitia.github@gmail.com>
2021-09-05 15:04:33 -0500

    order by id

--- routes/order.js
+++ routes/order.js
@@ -3,15 +3,30 @@ const ordersRouter = express.Router();
 const db = require('../db');
 
 // Create new order
-ordersRouter.post('/',);
+//ordersRouter.post('/',);
+
+// View all orders (for specific user)
+ordersRouter.get('/', async (req, res, next) => {
+    // need to pass session information to get userId for this call (TO DO)
+
+    
+});
+
 
 // Retrieve data from existing order
-ordersRouter.get('/:id',);
+ordersRouter.get('/:id', async(req, res, next) => {
+    const orderId = req.params.id;
+    const order = await db.query(`SELECT * FROM public.orders WHERE orderId = $1`, [orderId]);
+    res.send(order.rows);  
+});
 
 // Modify existing order
-ordersRouter.put('/:id',);
+//ordersRouter.put('/:id',);
 
 // Delete existing order
-ordersRouter.delete('/:id',);
+//ordersRouter.delete('/:id',);
+
+
+
 
 module.exports = ordersRouter;
