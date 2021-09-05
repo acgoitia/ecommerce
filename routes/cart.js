63f370e5ae7fad8b0728ee1eb76160552b78caa9
acgoitia <acgoitia.github@gmail.com>
2021-09-05 15:37:08 -0500

    cart and order in progress

--- routes/cart.js
+++ routes/cart.js
@@ -2,8 +2,24 @@ const express = require('express');
 const cartsRouter = express.Router();
 const db = require('../db');
 
+// get current cartId
+const currentCartId = async (req, res, next) => {
+    const idResponse = await db.query('SELECT MAX(cartId) FROM public.cart_product');
+    const current_id = idResponse.rows[0].max;
+    let new_id;
+    if (current_id) {
+      req.new_id = current_id + 1;
+    } else {
+      req.new_id = 1; 
+    }
+    next();
+}
+
+
 // Create new cart
-cartsRouter.post('/',);
+cartsRouter.post('/', currentCartId, async(req, res, next) => {
+
+});
 
 // Retrieve data from existing cart
 cartsRouter.get('/:id',);
