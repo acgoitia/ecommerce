e221b298044192d4df31ed7691bff5fb917d6bf8
acgoitia <acgoitia.github@gmail.com>
2021-09-12 21:24:53 -0500

    complete users routers

--- routes/users.js
+++ routes/users.js
@@ -22,16 +22,53 @@ usersRouter.put('/myprofile', async (req, res, next) => {
     const { id } = req.user;
     const { first_name, last_name, email, password } = req.body;
     
-    //let statement = "";
-    //if(first_name){
-      const statement = `first_name = '${first_name}'`;
-    //}
 
+    // dynamically define which columns to update based on the BODY data
+    let statement = "";
+    if(first_name){
+      statement += `first_name = '${first_name}'`;
+    }
+
+    if(last_name){
+      if(statement){
+        statement += ", ";
+      }
+      statement += `last_name = '${last_name}'`;
+    }
+
+    if(email){
+      
+      // check that email is not registered
+      const emailResponse = await db.query('SELECT * FROM public.user WHERE email = $1', [email]);
+      var hasEmail;
+      if (emailResponse.rows[0]) {
+        hasEmail = emailResponse.rows[0].email;
+      }
+      if (hasEmail) {
+        const err = new Error('email already registered');
+        err.status = 400;
+        return next(err);
+      } 
+
+      // if email not currently in database:
+      if(statement){
+        statement += ", ";
+      }
+      statement += `email = '${email}'`;
+    }
+
+    if(password){
+      if(statement){
+        statement += ", ";
+      }
+      statement += `password = '${password}'`;
+    }
+
+    // log date
+    statement += `, modified = (SELECT LOCALTIMESTAMP)`;
+
+    // update database
     const response = await db.query(`UPDATE public.user SET ${statement} WHERE id = ${id}`)
-
-    //const response = await db.query(`SELECT * FROM public.user WHERE id = $1`, [id]);
-    //const user = response.rows[0];
-
 
 
     res.send('user updated');
