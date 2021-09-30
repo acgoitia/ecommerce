3f5970079c42a1bc60f65a66bfbeb00235acf62d
acgoitia <acgoitia.github@gmail.com>
2021-09-30 13:10:13 -0500

    cart page in progress

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import './cart.css';
 
 
 function Cart (props) {
+
+    const {isLoggedIn} = useSelector((state) => state.login);
+    const [cart, setCart] = useState([]);
+
+    const getCart = async () => {
+        const response = await fetch("http://localhost:4001/api/users/myprofile/cart", {
+            method: 'GET',
+            credentials: 'include', // need to include in order for fetch method to send the cookie
+            mode: 'cors'
+        });
+        const jsonData = await response.json();
+        return jsonData;
+    };
+
+    useEffect(() => {
+        setCart(getCart())
+    }, [])
+
+
     return(
         <div>
-            <p>Display Cart only for loged-in users</p>
+            {isLoggedIn ? 
+                <div>
+                    <table>
+                        <tr>
+                            <th>Quantity</th>
+                            <th>Item</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                        </tr>
+                        {
+                            // cart.map((item) => {
+                            //     <tr>
+                            //         <td>{item.quantity}</td>
+                            //         <td></td>
+                            //         <td></td>
+                            //         <td>{item.price}</td>
+                            //         <td></td>
+                            //     </tr>
+                            // })
+                        }
+                    </table>
+                </div> : 
+                <Redirect to="/" />
+            }           
         </div>
     );
 }
