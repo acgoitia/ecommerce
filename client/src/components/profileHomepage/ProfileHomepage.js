b6c65ee352690b0512ebc010f83dd583c1552729
acgoitia <acgoitia.github@gmail.com>
2021-10-01 16:25:53 -0500

    order history implemented

--- client/src/components/profileHomepage/ProfileHomepage.js
+++ client/src/components/profileHomepage/ProfileHomepage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useSelector } from "react-redux";
 import { Redirect, useHistory } from "react-router-dom";
 import './profileHomepage.css';
@@ -7,10 +7,30 @@ function ProfileHomepage (props) {
     
     const history = useHistory();
     const {isLoggedIn, profile} = useSelector((state) => state.login);
-    
+    const [orders, setOrders] = useState([]);
+
+    const getOrders = async () => {
+        const response = await fetch("http://localhost:4001/api/users/myprofile/orders", {
+            method: 'GET',
+            credentials: 'include', // need to include in order for fetch method to send the cookie
+            mode: 'cors'
+        });
+        const jsonData = await response.json();
+        console.log(jsonData)
+        setOrders(jsonData);
+    }
+
+    useEffect(() => {
+        getOrders();
+    }, [])
+
     const handleEdit = (e) => {
         history.push('/myprofile/userinfo')
     };
+
+    const handleView = ({target}) => {
+        history.push(`/myprofile/order/${target.value}`)
+    }
 
     return (
         <div>
@@ -25,7 +45,31 @@ function ProfileHomepage (props) {
                     </div>
                     <div className="order-history">
                         <h2>Order History</h2>
-                        <p>TBD add all order rows</p>
+                        <table>
+                            <tr>
+                                <th>Date</th>
+                                <th>Order #</th>
+                                <th>Status</th>
+                                <th>Total</th>
+                                <th></th>
+                            </tr>
+                            {
+                                orders.map((order) => {
+                                    const t = order.created.split(/[- : T]/);
+                                    const d = new Date(Date.UTC(t[0], t[1]-1, parseInt(t[2])+1));
+                                    
+                                    return (<div>
+                                        <tr>
+                                            <th>{d.toDateString()}</th>
+                                            <th>#{order.id}</th>
+                                            <th>{order.status}</th>
+                                            <th>${order.total}</th>
+                                            <th><button onClick={handleView} value={order.id}>View</button></th>
+                                        </tr>       
+                                    </div>);
+                                })
+                            }
+                        </table>
                     </div>
                 </div> :
                 <Redirect to="/" ></Redirect>
@@ -36,4 +80,3 @@ function ProfileHomepage (props) {
 
 export default ProfileHomepage;
 
-// Will redirect to login route if not logged in
