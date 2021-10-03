5692a02e661c699e22c19427ed8bfa6c94116ba2
acgoitia <acgoitia.github@gmail.com>
2021-10-03 13:48:46 -0500

    ready for final deployment

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS public.user (
       id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
       first_name VARCHAR(50),
       last_name VARCHAR(50),
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE IF NOT EXISTS public.categories (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     category        VARCHAR(50)     NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS public.products (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     name            VARCHAR(50)     NOT NULL,
     price           BIGINT          NOT NULL,
@@ -23,45 +23,45 @@ CREATE TABLE IF NOT EXISTS products (
     categoryid      INT,
     image_url       VARCHAR(255),
     description     VARCHAR(255)     NOT NULL,
-    FOREIGN KEY (categoryid) REFERENCES categories(id)
+    FOREIGN KEY (categoryid) REFERENCES public.categories(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS public.orders (
     id              INT                             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     user_id         INT                             NOT NULL,
     created         TIMESTAMP WITHOUT TIME ZONE     NOT NULL,
     modified        TIMESTAMP WITHOUT TIME ZONE     NOT NULL,
     status          VARCHAR(50)                     NOT NULL,
     total           BIGINT                          NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES public.user(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS order_items (
+CREATE TABLE IF NOT EXISTS public.order_items (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     order_id        INT             NOT NULL,
     pdoruct_id      INT             NOT NULL,
     price           BIGINT          NOT NULL,
     quantity        INT             NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (pdoruct_id) REFERENCES products(id)
+    FOREIGN KEY (order_id) REFERENCES public.orders(id),
+    FOREIGN KEY (pdoruct_id) REFERENCES public.products(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS cart (
+CREATE TABLE IF NOT EXISTS public.cart (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     user_id         INT             NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES public.user(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS cart_items (
+CREATE TABLE IF NOT EXISTS public.cart_items (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     product_id      INT             NOT NULL,
     cart_id         INT             NOT NULL,
     price           BIGINT          NOT NULL,
     quantity        INT             NOT NULL,
-    FOREIGN KEY (cart_id) REFERENCES cart(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    FOREIGN KEY (cart_id) REFERENCES public.cart(id),
+    FOREIGN KEY (product_id) REFERENCES public.products(id)
 );
