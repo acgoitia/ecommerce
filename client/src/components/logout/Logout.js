156f5d7a2f5ebc4999ff85f13c85481326bb25e4
acgoitia <acgoitia.github@gmail.com>
2021-10-02 21:45:14 -0500

    sytling complete

--- client/src/components/logout/Logout.js
+++ client/src/components/logout/Logout.js
@@ -4,8 +4,8 @@ import './logout.css';
 
 export default function Logout(props) {
     return (
-        <div>
-            <p>successfully logged out</p>
+        <div className="logout">
+            <p>Successfully logged out!</p>
             <p>Return to <Link to="/">Home</Link></p>
         </div>
     )
