5a2e247c5b0b898b40c2afd841c564e5194e6cb0
acgoitia <acgoitia.github@gmail.com>
2021-10-03 20:15:49 -0500

    added ssl rejectUnauthorized to connection string

--- db/index.js
+++ db/index.js
@@ -8,13 +8,16 @@ const devConfig = {
     port: process.env.PGPORT
 };
 
-// const prodConfig = {
-//     connectionString: process.env.DATABASE_URL // comes from heroku addon
-// };
+const prodConfig = {
+    connectionString: process.env.DATABASE_URL, // comes from heroku addon
+    ssl: {
+      rejectUnauthorized: false
+    }
+};
 
-const prodConfig = process.env.DATABASE_URL; // comes from heroku addon
+//const prodConfig = process.env.DATABASE_URL; // comes from heroku addon
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? {prodConfig,} : devConfig);
+const pool = new Pool(process.env.NODE_ENV === "production" ? prodConfig : devConfig);
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
